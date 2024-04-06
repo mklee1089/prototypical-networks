d4e70e794b5aca0d5dbc4147d78611b2693c447a
mklee1089 <edward108928@gmail.com>
2024-04-06 11:12:03 +0800

    change small hyperparameters

--- scripts/train/few_shot/run_train.py
+++ scripts/train/few_shot/run_train.py
@@ -22,10 +22,10 @@ parser.add_argument('--data.test_way', type=int, default=5, metavar='TESTWAY',
 parser.add_argument('--data.test_shot', type=int, default=0, metavar='TESTSHOT',
                     help="number of support examples per class in test. 0 means same as data.shot (default: 0)")
 parser.add_argument('--data.test_query', type=int, default=15, metavar='TESTQUERY',
-                    help="number of query examples per class in test. 0 means same as data.query (default: 15)")
-parser.add_argument('--data.train_episodes', type=int, default=100, metavar='NTRAIN',
+                    help="number of query examples per class in test. 0 means same as data.query (default: 15)")i
+parser.add_argument('--data.train_episodes', type=int, default=50, metavar='NTRAIN',
                     help="number of train episodes per epoch (default: 100)")
-parser.add_argument('--data.test_episodes', type=int, default=100, metavar='NTEST',
+parser.add_argument('--data.test_episodes', type=int, default=50, metavar='NTEST',
                     help="number of test episodes per epoch (default: 100)")
 parser.add_argument('--data.trainval', action='store_true', help="run in train+validation mode (default: False)")
 parser.add_argument('--data.sequential', action='store_true', help="use sequential sampler instead of episodic (default: False)")
@@ -43,7 +43,7 @@ parser.add_argument('--model.z_dim', type=int, default=64, metavar='ZDIM',
                     help="dimensionality of input images (default: 64)")
 
 # train args
-parser.add_argument('--train.epochs', type=int, default=10000, metavar='NEPOCHS',
+parser.add_argument('--train.epochs', type=int, default=1000, metavar='NEPOCHS',
                     help='number of epochs to train (default: 10000)')
 parser.add_argument('--train.optim_method', type=str, default='Adam', metavar='OPTIM',
                     help='optimization method (default: Adam)')
@@ -54,7 +54,7 @@ parser.add_argument('--train.decay_every', type=int, default=20, metavar='LRDECA
 default_weight_decay = 0.0
 parser.add_argument('--train.weight_decay', type=float, default=default_weight_decay, metavar='WD',
                     help="weight decay (default: {:f})".format(default_weight_decay))
-parser.add_argument('--train.patience', type=int, default=200, metavar='PATIENCE',
+parser.add_argument('--train.patience', type=int, default=20, metavar='PATIENCE',
                     help='number of epochs to wait before validation improvement (default: 1000)')
 
 # log args
